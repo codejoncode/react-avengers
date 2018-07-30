913d52fb0c4307423223f3b65391feb4bd1ae557
codejoncode <jonathanjholloway@gmail.com>
2018-07-30 11:54:15 -0500

    new react-avengers app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {name: "Jonathan Holloway"}
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Avengers: {this.state.name} </h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
